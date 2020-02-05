03ef598c46fd9c442c6224728969b92154ee5832
Hannah Harrington <hannahharrington@outlook.com>
2020-02-05 09:21:01 +0000

    Automatically backed up by Learn

---
+++ learning-comments.rb
@@ -0,0 +1,9 @@
+run_code_inside = true
+puts "Code before if...end"
+if run _code_inside 
+  puts "code inside"
+end
+puts "Code after if..end"
+
+answer = "flamboyance"
+puts "A group of flamingos is called a #{answer}."
